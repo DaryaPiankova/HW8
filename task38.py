e3b94f60c301dcb1dd8472cc5145175545acbe63
Daria <darya.piankova@yandex.ru>
2023-05-27 16:48:02 +0500

    task38

--- task38.py
+++ task38.py
@@ -1,46 +1,46 @@
 import sys
 import io
 
-def readfile(filename):
+
+def readFile(filename):
     data = [i.split() for i in open(filename, 'r', encoding='utf-8')]
     return data
 
 
-def printinfo(data):
+def printInfo(data):
     for i in data:
-        print(i)
+        print(f'{i}\n')
 
 
 def exit(data):
     print('До свидания!')
 
 
-def writefile(data):
+def writeFile(data):
     new = input('Введите данные ').split()
     with open('tel.txt', 'a', encoding='utf-8') as f:
         f.writelines("%s\n" % line for line in new)
-    
 
-def finddata(data):
-    word = input('введите характеристику')
+
+def findData(data):
+    word = input('введите характеристику: ')
     with io.open('tel.txt', encoding='utf-8') as file:
         for line in file:
             if word in line:
                 print(line, end='')
 
 
-
 def main():
     my_choice = -1
-    data = readfile('tel.txt')
+    data = readFile('tel.txt')
     while my_choice != 0:
         print('Выберите одно из действий:')
         print('1 - вывести инфо на экран')
-        print('2 - произвести экпорт данных')
-        print('3 - найти строку')
+        print('2 - вписать данные в файл')
+        print('3 - найти строку по характеристрике')
         print('0 - выход из программы')
         my_choice = int(input())
-        operations = {0: exit, 1: printinfo, 2: writefile, 3: finddata}
+        operations = {0: exit, 1: printInfo, 2: writeFile, 3: findData}
         operations[my_choice](data)
 
 
