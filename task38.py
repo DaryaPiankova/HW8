458876c27a23dcaa8243039a99b64a2921b98119
Daria <darya.piankova@yandex.ru>
2023-05-27 17:40:16 +0500

    task38

--- task38.py
+++ task38.py
@@ -1,5 +1,6 @@
 import sys
 import io
+import re
 
 
 def readFile(filename):
@@ -30,6 +31,30 @@ def findData(data):
                 print(line, end='')
 
 
+def changeData(data):
+    oldData = input(
+        'введите данные, которые хотите изменить ')
+    newData = input('введите новые данные ')
+
+    with open('tel.txt', 'r', encoding='utf-8') as f:
+        old_data = f.read()
+        new_data = old_data.replace(oldData, newData)
+
+    with open('tel.txt', 'w', encoding='utf-8') as f:
+        f.write(new_data)
+
+
+def deleteData(data):
+    dataToDelete = input(
+        'Введите данные для удаления ')
+    with open("tel.txt", "r", encoding='utf-8') as f:
+        lines = f.readlines()
+    with open("tel.txt", "w", encoding='utf-8') as f:
+        for line in lines:
+            if line.strip("\n") != dataToDelete:
+                f.write(line)
+
+
 def main():
     my_choice = -1
     data = readFile('tel.txt')
@@ -38,9 +63,12 @@ def main():
         print('1 - вывести инфо на экран')
         print('2 - вписать данные в файл')
         print('3 - найти строку по характеристрике')
+        print('4 - изменение данных')
+        print('5 - удаление данных')
         print('0 - выход из программы')
         my_choice = int(input())
-        operations = {0: exit, 1: printInfo, 2: writeFile, 3: findData}
+        operations = {0: exit, 1: printInfo, 2: writeFile,
+                      3: findData, 4: changeData, 5: deleteData}
         operations[my_choice](data)
 
 
